4cd229eb34c04c3716aa6567e5f153c810ff876e
heisqueenson9 <eqeduafo@st.ug.edu.gh>
2025-08-04 19:46:49 -0700

    Created IDriveable interface with implementations in Sedan and PedalBike classes

---
+++ InterfaceDemo.cs/InterfaceDemo.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AssignmentApp
+{
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    // Demo class
+    class InterfaceDemo
+    {
+        public static void Run()
+        {
+            IMovable car = new Car();
+            IMovable bike = new Bicycle();
+
+            car.Move();
+            bike.Move();
+        }
+
+        // Entry point
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
