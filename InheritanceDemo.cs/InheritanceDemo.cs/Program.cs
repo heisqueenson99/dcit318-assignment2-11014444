7bcd5f27de2a692791b176d442bd3011b30d046e
heisqueenson9 <eqeduafo@st.ug.edu.gh>
2025-08-04 19:33:38 -0700

    Implemented inheritance

---
+++ InheritanceDemo.cs/InheritanceDemo.cs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AssignmentApp
+{
+    // Base class
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    // Demo class to run the code
+    public class InheritanceDemo
+    {
+        public static void Run()
+        {
+            Animal a = new Animal();
+            Animal d = new Dog();
+            Animal c = new Cat();
+
+            a.MakeSound();  // Output: Some generic sound
+            d.MakeSound();  // Output: Bark
+            c.MakeSound();  // Output: Meow
+        }
+
+        // Entry point of the program
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
